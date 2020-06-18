0da9986353600610d76da68d3bc9d3590a91184f
Carlos Prieto <carlospm86@gmail.com>
2020-06-18 17:03:28 +0200

    customize searcher styles

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -21,6 +21,12 @@ export class HomePageComponent implements OnInit {
     this.dates = this._datesService.getLastDays()
     this.dates = this.dates.map(d => '&dates[]='.concat(d));
     this.datesToSend = this._datesService.convertToString(this.dates);
+    this.dates = ["&dates[]=2018-04-18" // Eliminar cuando se deje de hacer pruebas
+      , "&dates[]=2018-04-17" // Eliminar cuando se deje de hacer pruebas
+      , "&dates[]=2018-04-16" // Eliminar cuando se deje de hacer pruebas
+      , "&dates[]=2018-04-15" // Eliminar cuando se deje de hacer pruebas
+      , "&dates[]=2018-04-14"]; // Eliminar cuando se deje de hacer pruebas
+    this.datesToSend = '&dates[]=2018-04-18&dates[]=2018-04-17&dates[]=2018-04-16&dates[]=2018-04-15&dates[]=2018-04-14'; // Eliminar cuando se deje de hacer pruebas
     this._teamService.getMatchYesterday(this.datesToSend).subscribe(res => {
       this.games = this._datesService.getLastResuts(res.data, this.dates);
     })
